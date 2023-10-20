8609c47b17808e49ca52d7b09fc694148cc14239
smsmaurya <sumeshmaurya786@gmail.com>
2023-10-20 17:31:26 +0530

    [Sumesh]:- Check Employee is present or absent.

--- src/com/bridgeLabz/CheckEmployeePresenceUC1.java
+++ src/com/bridgeLabz/CheckEmployeePresenceUC1.java
@@ -2,6 +2,13 @@ package com.bridgeLabz;
 
 public class CheckEmployeePresenceUC1 {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if(empCheck==IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
