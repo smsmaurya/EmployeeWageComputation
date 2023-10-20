98e3dc54aa1ca61d035190efc2c8c900ee5a55c3
smsmaurya <sumeshmaurya786@gmail.com>
2023-10-20 17:27:36 +0530

    [Sumesh]:- Welcome to employee wage

---
+++ src/com/bridgeLabz/CheckEmployeePresenceUC1.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+
+public class CheckEmployeePresenceUC1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
