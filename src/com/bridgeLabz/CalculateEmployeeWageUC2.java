962c7b2dc6351857bb79764ed0990838dd1ff077
smsmaurya <sumeshmaurya786@gmail.com>
2023-10-20 17:53:31 +0530

    [Sumesh]:- Calculate employee wage .

---
+++ src/com/bridgeLabz/CalculateEmployeeWageUC2.java
@@ -0,0 +1,24 @@
+package com.bridgeLabz;
+
+public class CalculateEmployeeWageUC2 {
+    public static void main(String[] args) {
+        // constants
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        // variables
+        int empHrs;
+        int empWage;
+
+        //computation
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if(empCheck==IS_FULL_TIME){
+            empHrs = 8;
+        }
+        else{
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage : "+ empWage);
+    }
+}
